111db39ea0aa8bd6d5efbd466624e2aefec560d5
hibiking-0422 <50702357+hibiking-0422@users.noreply.github.com>
2019-12-10 16:03:28 +0900

    fwaefaew

--- golang_install.sh
+++ golang_install.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
+<<commentout
+$ bash <(curl -s https://raw.githubusercontent.com/hibiking-0422/golang_install/master/golang_install.sh)
+commentout
+
+#golang install
 curl -L -O https://dl.google.com/go/go1.12.5.linux-amd64.tar.g
 
+#解凍
 sudo tar -C /usr/local -xf go1.12.5.linux-amd64.tar.gz
 
+#path
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bash_profile
 
+#goget install ,リポジトリのやつ
 sudo yum install -y git mercurial subversion
 
+#path
 echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> ~/.bash_profile
